078fda99ae201d725c346270d9f4c97c0286bd05
rioX432 <omooooori432@gmail.com>
2026-02-27 14:16:28 +0900

    Add accent color DI registrations to SettingsViewModel

--- androidApp/src/main/kotlin/com/riox432/civitdeck/CivitDeckApplication.kt
+++ androidApp/src/main/kotlin/com/riox432/civitdeck/CivitDeckApplication.kt
@@ -109,6 +109,7 @@ val androidModule = module {
             get(), get(), get(), get(), get(), get(),
             get(), get(), get(), get(), get(), get(),
             get(), get(), get(), get(),
+            get(), get(), get(), get(),
         )
     }
     viewModel { ModelFileBrowserViewModel(get(), get(), get(), get(), get(), get()) }
