c1daa56f95548cf461a1ff4edf876549c5ca4361
rioX432 <omooooori432@gmail.com>
2026-03-16 10:28:53 +0900

    Remove unused dp import (detekt autoCorrect)

--- androidApp/src/main/kotlin/com/riox432/civitdeck/ui/dataset/DuplicateReviewScreen.kt
+++ androidApp/src/main/kotlin/com/riox432/civitdeck/ui/dataset/DuplicateReviewScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.riox432.civitdeck.R
 import com.riox432.civitdeck.domain.model.DatasetImage
